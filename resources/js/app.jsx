1a4ca2dcc7edbe0fca8c541f0c2ac29704f7084c
Oscar <oscarbarcenas417@gmail.com>
2023-10-23 10:58:57 -0600

    Commit Login

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -16,6 +16,7 @@ import './bootstrap';
 
 import ReactDOM from 'react-dom/client';
 import Main from './components/Main'
+
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('app')).render(
--- resources/js/components/Main.jsx
+++ resources/js/components/Main.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import Card_C from "./Card_C";
 import ListCards from "./ListCards";
 import Menu from "./Menu";
+import Login from "./login";
 import { Navigate, Routes, Route } from "react-router-dom";
 
 function Main() {
     return(
         <Routes>
-            <Route path="/example-app/public/" element={<Menu/>}>
-                <Route path="card" element={<Card_C/>}/>
-                <Route path="listcards" element={<ListCards/>}/>
-                <Route path="*" element={<Navigate replace to="/" />}/>
+            <Route path="/example-app/public/" element={<Login/>}>
+            
+            <Route path="card" element={<Card_C/>}/>
+            <Route path="listcards" element={<ListCards/>}/>
+            <Route path="*" element={<Navigate replace to="/" />}/>
             </Route>
         </Routes>
     )
